a7e97b2d53251f559ef3430d54cdd84b6543d000
TanManWeb <59107742+TanManWeb@users.noreply.github.com>
2019-12-30 12:45:11 -0800

    Changed conditional statements

--- localhost-conditional-tag.php
+++ localhost-conditional-tag.php
@@ -4,12 +4,22 @@ Plugin Name: Localhost Conditional Tag
 Description: A simple and lightweight plugin that allows you to utilize the localhost conditional tag in the PHP programming of your WordPress theme.
 Author: TanManWeb
 Author URI: https://github.com/TanManWeb/
-Version: 1.0.0
+Version: 1.1.0
 Text Domain: localhost-conditional-tag
 */
 
 if ( ! function_exists( 'is_localhost' ) ) {
-	function is_localhost($whitelist = ['127.0.0.1', 'localhost', '::1']) {
-		return in_array($_SERVER['SERVER_NAME'], $whitelist);
-	}	
+
+	function is_localhost() {
+		
+		$server_name = $_SERVER['SERVER_NAME'];
+		$server_addr = $_SERVER['SERVER_ADDR'];
+
+		if( strpos( $server_name, 'localhost' ) !== false || $server_addr == "::1" || $server_addr == "127.0.0.1" ) {
+			$env = "localhost";			
+		}
+		return $env;	
+
+	}
+
 }
